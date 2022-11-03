a189c4d7143d4a3e53bb10fad4fc26cd0b0598e8
Rafael Silva <rasilva.1986@gmail.com>
2022-11-03 17:54:39 +0100

    Create project structure

--- src/Main.py
+++ src/Main.py
@@ -1,1 +1,6 @@
-print('Hello, there!')
+from classes.Bank import Bank
+
+
+if __name__ == '__main__':
+    b1 = Bank(12, 12)
+    b1.print_bank()
---
+++ src/classes/Bank.py
@@ -0,0 +1,6 @@
+class Bank:
+    def __init__(self, x):
+        self.x = x
+
+    def print_bank(self):
+        print('hello, bank')
